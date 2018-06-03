d95eb71008ab85baf76024f84034d6af3def7bfe
disconaked <nolan.klinke@gmail.com>
2018-06-03 09:35:32 -0400

    throwing things into functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,29 +2,34 @@
 var wins= 0;
 var losses= 0;
 var guessesLeft= 9;
-var guessesSoFar= "";
+var guessesSoFar= [];
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+computerChoice = letters[Math.floor(Math.random() * letters.length)];
 
-document.onkeydown = function(event) {
-
-    var userGuess = event.key;
-
+function refreshLetter() {
     var computerChoice = letters[Math.floor(Math.random() * letters.length)];
-
-    if (userGuess === computerChoice) {
-        wins++;
-        guessesLeft= 9;
-        $("#plusWins").text(wins);
-    } else {
-        guessesLeft--;
-        $("#guessLeft").text(guessesLeft);
-        $("#guesses").text(userGuess);
-    }
+    console.log(computerChoice); 
 };
-/*
-var newH1 = $("<H1>");
-        newH1.text(randomNumber);
-        newH1.attr("class", "text-center");
 
-          $("#random-number").prepend(newH1);
-*/
+refreshLetter();
+
+function updateGuessesLeft() {
+    $("#guessLeft").text(guessesLeft);
+}
+
+function updateWins() {
+    $("#plusWins").text(wins);
+};
+
+function updateLosses() {
+    $("#plusLoss").text(losses);
+};
+
+function reset () {
+    guessesLeft =9;
+
+};
+
+updateGuessesLeft();
+
+
